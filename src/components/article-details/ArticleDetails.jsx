517f040e2d41c10aaec0c5f49d43c8588a87bef5
Vikicha089 <vbanchevf@gmail.com>
2025-12-03 01:02:54 +0200

    Add create comment functionality

--- src/components/article-details/ArticleDetails.jsx
+++ src/components/article-details/ArticleDetails.jsx
@@ -164,7 +164,7 @@ export default function ArticleDetails() {
                         </div>
 
                         {/* Comments Section (Simplified) */}
-                        <CommentSection />
+                        <CommentSection articleId={articleId} />
                     </div>
 
                     {/* Right Column: Sidebar (Standard) */}
--- src/components/article-details/CommentSection.jsx
+++ src/components/article-details/CommentSection.jsx
@@ -1,7 +1,39 @@
+import { useEffect, useRef, useState } from "react";
+import useForm from "../../hooks/useForm.js";
 import useRequest from "../../hooks/useRequest.js";
 
-export default function CommentSection() {
-    const { data: comments } = useRequest('/data/comments', []);
+export default function CommentSection({ articleId }) {
+    // const [comments, setComments] = useState([]);
+    const formRef = useRef();
+    const urlParams = new URLSearchParams({
+        where: `_articleId="${articleId}"`,
+        load: `author=_ownerId:users`
+    })
+    const { request, data: comments, setData: setComments, setNewData } = useRequest(`/data/comments?${urlParams}`, []);
+
+
+    const submitCommentHandler = async (values) => {
+        const { comment } = values;
+        const newComment = {
+            comment,
+            _articleId: articleId,
+        }
+        try {
+            const result = await request('/data/comments', "POST", newComment);
+            setComments(state => [...state, result]);
+            resetForm();
+        } catch (err) {
+            alert(err.message);
+        }
+    }
+
+    const {
+        register,
+        formAction,
+        resetForm
+    } = useForm(submitCommentHandler, {
+        comment: '',
+    })
 
     return (
         <div className="mt-16">
@@ -10,23 +42,24 @@ export default function CommentSection() {
             </h3>
 
             {/* Input */}
-            <div className="flex gap-4 mb-10">
+            <form action={formAction} ref={formRef} className="flex gap-4 mb-10">
                 <div className="w-10 h-10 rounded-full bg-gray-200 flex-shrink-0"></div>
                 <div className="flex-grow relative">
                     <textarea
                         rows="3"
                         placeholder="Join the debate..."
+                        {...register('comment')}
                         className="w-full bg-gray-50 border border-gray-200 p-3 focus:border-[#e10600] focus:ring-0 outline-none transition-all resize-none text-sm rounded-sm"
                     ></textarea>
-                    <button className="absolute bottom-3 right-3 bg-[#15151e] text-white px-4 py-1 text-[10px] font-bold uppercase tracking-wider hover:bg-[#e10600] transition-colors rounded-sm">
+                    <button type="submit" className="absolute bottom-3 right-3 bg-[#15151e] text-white px-4 py-1 text-[10px] font-bold uppercase tracking-wider hover:bg-[#e10600] transition-colors rounded-sm">
                         Post
                     </button>
                 </div>
-            </div>
+            </form>
 
             {/* Comment List */}
             <div className="space-y-6">
-                {comments.map(com => <div className="flex gap-4">
+                {comments.map(com => (<div className="flex gap-4" key={com._id}>
                     <img src="https://api.dicebear.com/7.x/avataaars/svg?seed=John" alt="User" className="w-10 h-10 rounded-full bg-gray-100" />
                     <div>
                         <div className="flex items-center gap-2 mb-1">
@@ -39,7 +72,7 @@ export default function CommentSection() {
                             <button className="text-xs font-bold text-gray-400 hover:text-[#e10600]">Like (5)</button>
                         </div>
                     </div>
-                </div>)}
+                </div>))}
 
             </div>
         </div>
