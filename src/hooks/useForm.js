2d911eb39491fd38a2a5e7b15c5313139ec0ae5a
Vikicha089 <vbanchevf@gmail.com>
2025-12-04 23:14:10 +0200

    Start transformation of useForm to work with image upload
    - display preview image
    - //TODO upload image to firebase and return url

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -2,13 +2,36 @@ import { useState } from "react";
 
 export default function useForm(callback, initialValues) {
     const [values, setValues] = useState(initialValues);
+    const [imagePreview, setImagePreview] = useState('');
 
     const changeHandler = (e) => {
+        debugger
+    if (e.target.name === 'image' && e.target.files) {
+        const file = e.target.files[0];
+        console.log(file);
+        
+        if (file) {
+            console.log('dasda');
+            // A. Preview: Създава blob URL (вече го правите)
+            const objectUrl = URL.createObjectURL(file);
+            
+            setImagePreview(objectUrl);
+
+            setValues(state => ({
+                ...state,
+                'image': 'UPLOADING_PLACEHOLDER_URL' 
+            }));
+        }
+
+    } 
+    else {
+        setImagePreview(e.target.name === 'image' ? e.target.value : imagePreview);
         setValues(state => ({
             ...state,
             [e.target.name]: e.target.value
-        }))
+        }));
     }
+}
 
     const formAction = async (formData) => {
         await callback(values, formData)
@@ -21,6 +44,12 @@ export default function useForm(callback, initialValues) {
             value: values[fieldname],
         }
     }
+    const imageUpload = (fieldname) => {
+        return {
+            name: fieldname,
+            onChange: changeHandler,
+        }
+    }
 
     const resetForm = () => {
         setValues(initialValues)
@@ -28,9 +57,11 @@ export default function useForm(callback, initialValues) {
 
     return {
         values,
+        imageUpload,
         changeHandler,
         resetForm,
         register,
         formAction,
+        imagePreview
     }
 }
