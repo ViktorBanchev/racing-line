b14ab710f2c647d059877cf4d942eb19794d51c3
Vikicha089 <vbanchevf@gmail.com>
2025-12-08 16:40:09 +0200

    Use Optimistic on comment creation

--- src/components/article-details/CommentSection.jsx
+++ src/components/article-details/CommentSection.jsx
@@ -1,7 +1,8 @@
-import { useContext, useRef } from "react";
+import { startTransition, useContext, useOptimistic, useRef } from "react";
 import useForm from "../../hooks/useForm.js";
 import useRequest from "../../hooks/useRequest.js";
 import UserContext from "../../contexts/userContext.jsx";
+import { v4 as uuid } from 'uuid'
 
 export default function CommentSection({ articleId }) {
     const { user } = useContext(UserContext);
@@ -12,22 +13,23 @@ export default function CommentSection({ articleId }) {
         load: `author=_ownerId:users`
     })
     const { request, data: comments, setData: setComments } = useRequest(`/data/comments?${urlParams}`, []);
-
+    const [optimisticComments, setOptimisticComments] = useOptimistic(comments, (state, action) => [...state, action])
 
     const submitCommentHandler = async (values) => {
         const { comment } = values;
         const newComment = {
+            _id: uuid(),
             comment,
             _articleId: articleId,
+            author: user
         }
+        setOptimisticComments({ ...newComment, optimistic: true });
         try {
             const result = await request('/data/comments', "POST", newComment);
-            setComments(state => [...state, {
-                ...result,
-                'author': {
-                    'username': user.username
-                }
-            }]);
+            startTransition(() => {
+                setComments(state => [...state, result]);
+            })
+
             resetForm();
         } catch (err) {
             alert(err.message);
@@ -66,20 +68,23 @@ export default function CommentSection({ articleId }) {
 
             {/* Comment List */}
             <div className="space-y-6">
-                {comments.map(com => (<div className="flex gap-4" key={com._id}>
-                    <img src="https://api.dicebear.com/7.x/avataaars/svg?seed=John" alt="User" className="w-10 h-10 rounded-full bg-gray-100" />
-                    <div>
-                        <div className="flex items-center gap-2 mb-1">
-                            <span className="font-bold text-sm text-[#15151e]">{com.author?.username}</span>
-                            <span className="text-xs text-gray-400">2 hours ago</span>
+                {optimisticComments.map(com => (
+                    <div className="flex gap-4" key={com._id}>
+                        <img src={com.author.image} alt="User" className="w-10 h-10 rounded-full bg-gray-100" />
+                        <div>
+                            <div className="flex items-center gap-2 mb-1">
+                                <span className="font-bold text-sm text-[#15151e]">{com.author?.username}</span>
+                                <span className="text-xs text-gray-400">2 hours ago</span>
+                            </div>
+                            <p className="text-sm text-gray-700">{com.comment}</p>
+                            <div className="flex gap-4 mt-2">
+                                <button className="text-xs font-bold text-gray-400 hover:text-[#e10600]">Reply</button>
+                                <button className="text-xs font-bold text-gray-400 hover:text-[#e10600]">Like (5)</button>
+                            </div>
                         </div>
-                        <p className="text-sm text-gray-700">{com.comment}</p>
-                        <div className="flex gap-4 mt-2">
-                            <button className="text-xs font-bold text-gray-400 hover:text-[#e10600]">Reply</button>
-                            <button className="text-xs font-bold text-gray-400 hover:text-[#e10600]">Like (5)</button>
-                        </div>
-                    </div>
-                </div>))}
+
+                        {com.optimistic && 'Sending'}
+                    </div>))}
 
             </div>
         </div>
