1b6b6d068164c24ffed2074b38e5c5adf39cfa18
Vikicha089 <vbanchevf@gmail.com>
2025-12-08 16:42:06 +0200

    remove unused import in userContext.jsx

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react"
+import { createContext } from "react"
 import useRequest from "../hooks/useRequest.js";
 import useLocalStorage from "../hooks/useLocalStorage.js";
 
