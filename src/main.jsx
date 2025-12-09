e0600f1642092efc0e7064e4eaddbcd789946be5
Vikicha089 <vbanchevf@gmail.com>
2025-12-09 19:06:46 +0200

    Remove own notification display

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,12 @@
-import {createRoot} from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import {BrowserRouter} from 'react-router'
-import {UserProvider} from './contexts/userContext.jsx'
-import {NotificationProvider} from "./contexts/NotificationsContext.jsx";
+import { BrowserRouter } from 'react-router'
+import { UserProvider } from './contexts/userContext.jsx'
 
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
         <UserProvider>
-            <NotificationProvider>
-                <App/>
-            </NotificationProvider>
+            <App />
         </UserProvider>
     </BrowserRouter>
 )
