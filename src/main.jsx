715ee3ba87ad5d29c1f8717d5806f3d60aa97120
Vikicha089 <vbanchevf@gmail.com>
2025-12-09 10:24:54 +0200

    Create notifications context

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
-import { createRoot } from 'react-dom/client'
+import {createRoot} from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router'
-import { UserProvider } from './contexts/userContext.jsx'
+import {BrowserRouter} from 'react-router'
+import {UserProvider} from './contexts/userContext.jsx'
+import {NotificationProvider} from "./contexts/NotificationsContext.jsx";
 
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
         <UserProvider>
-            <App />
+            <NotificationProvider>
+                <App/>
+            </NotificationProvider>
         </UserProvider>
     </BrowserRouter>
 )
