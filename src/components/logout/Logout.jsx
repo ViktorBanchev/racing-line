ac9b0439a1db6ffc1f6b769abd33385df0b0b400
Vikicha089 <vbanchevf@gmail.com>
2025-12-09 15:00:04 +0200

    Use new notifications

--- src/components/logout/Logout.jsx
+++ src/components/logout/Logout.jsx
@@ -1,15 +1,16 @@
 import { useContext } from "react";
 import { useNavigate } from "react-router";
 import UserContext from "../../contexts/userContext.jsx";
+import {useNotificationsContext} from "../../contexts/NotificationsContext.jsx";
 
 export default function Logout() {
     const navigate = useNavigate();
+    const {showNotification} = useNotificationsContext()
     const {logoutHandler} = useContext(UserContext);
     logoutHandler()
         .then(() => navigate('/'))
         .catch((err) => {
-            console.log(err.message);
-            alert('Problem with logout');
+            showNotification('Problem with logout!', 'error');
             navigate('/');
         })
 
