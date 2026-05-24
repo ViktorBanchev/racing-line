39e9035ca109e3bf3477f66a41057221f01c2b35
Vikicha089 <vbanchevf@gmail.com>
2026-05-24 21:45:26 +0300

    Create mongoose connection

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,18 @@
 import express from 'express';
 import cors from 'cors';
 import routes from './routes.js';
+import mongoose from 'mongoose';
 
 
 const app = express();
+
+try {
+    await mongoose.connect('mongodb://localhost:27017', {
+        dbName: 'racing-line',
+    })
+} catch (error) {
+    console.error('Error connecting to database!')
+}
 
 app.use(cors());
 app.use(express.json());
@@ -12,5 +21,5 @@ app.use('/api', routes);
 
 const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => {
-    console.log(`Server listening on port: ${PORT}`);
+    console.log(`Server listening on port: ${PORT}`)
 })
