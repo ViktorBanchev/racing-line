806478c4da51c6599bfd03f98161cdaa4f200a4e
Vikicha089 <vbanchevf@gmail.com>
2026-05-24 21:46:23 +0300

    Create auth controller with login and register

---
+++ server/src/controller/authController.ts
@@ -0,0 +1,68 @@
+import { Router, type Request, type Response } from "express";
+import type { LoginPayload, RegisterPayload } from "../types/user.types.js";
+import { loginUser, registerUser } from "../services/authService.js";
+import { generateToken } from "../utils/auth.service.js";
+
+const authController = Router();
+
+authController.post('/register', async (req: Request, res: Response) => {
+    const { username, email, password, confirmPassword, image }: RegisterPayload = req.body;
+
+    if (confirmPassword !== password) {
+        return res.status(400).json({ message: 'Password mismatch' });
+    }
+
+    try {
+        const user = await registerUser({ username, email, password, image });
+        const token = generateToken(user._id.toString())
+
+        const userResponse = {
+            _id: user.id,
+            username: user.username,
+            email: user.email,
+            image: user.image
+        }
+
+        res.status(201).json({
+            message: 'User registered',
+            data: {
+                user: userResponse,
+                accessToken: token
+            }
+        })
+    } catch (error: any) {
+        res.status(400).json({
+            message: error.message! || 'Error during register!'
+        })
+    }
+})
+
+authController.post('/login', async (req: Request, res: Response) => {
+    const { email, password }: LoginPayload = req.body;
+
+    try {
+        const user = await loginUser({ email, password });
+        const token = generateToken(user._id.toString());
+
+        const userResponse = {
+            _id: user.id,
+            username: user.username,
+            email: user.email,
+            image: user.image
+        }
+
+        res.status(200).json({
+            message: 'User logged in!',
+            data: {
+                accessToken: token,
+                user: userResponse
+            }
+        })
+    } catch (error: any) {
+        res.status(400).json({
+            message: error.message || 'Error during login!'
+        })
+    }
+})
+
+export default authController;
