00cdfe6491fed3f2ddbfb640a94c354b1ec17ba8
Vikicha089 <vbanchevf@gmail.com>
2026-05-25 01:23:12 +0300

    UPDATE Article service
    -Improve getAll
    - Add Creation
    - Add getOne

--- server/src/services/articleService.ts
+++ server/src/services/articleService.ts
@@ -1,5 +1,39 @@
-import Article from "../models/Article.js";
+import type { HydratedDocument, Types } from "mongoose";
+import Article, { type IArticle } from "../models/Article.js";
+import type { ArticlePayload, ArticleQueryOptions } from "../types/article.types.js";
 
-export async function getAllArticles() {
-    return await Article.find();
+export async function getAllArticles(options: ArticleQueryOptions) {
+    let query = Article.find();
+
+    if (options.sortBy) {
+        const [field, order] = options.sortBy.split(' ');
+        query = query.sort({ [field as string]: order === 'desc' ? -1 : 1 });
+    } else {
+        query = query.sort({ createdAt: -1 })
+    }
+
+    if (options.pageSize) {
+        query = query.limit(parseInt(options.pageSize));
+    }
+
+    if (options.load && options.load.includes('author')) {
+        query = query.populate('author', 'username email image')
+    }
+
+    return await query;
+}
+
+export async function createArticle(data: ArticlePayload & { author: Types.ObjectId }): Promise<HydratedDocument<IArticle>> {
+    const article = await Article.create(data);
+    return article;
+}
+
+export async function getArticle(articleId: string, options: ArticleQueryOptions) {
+    let query = Article.findById(articleId);
+
+    if (options.load && options.load.includes('author')) {
+        query = query.populate('author', 'username email image')
+    }
+
+    return await query;
 }
