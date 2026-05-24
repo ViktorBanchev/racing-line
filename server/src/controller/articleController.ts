812c49af70ee1d3b2081583a1cafefca5159f88a
Vikicha089 <vbanchevf@gmail.com>
2026-05-25 01:24:03 +0300

    Update articleController.ts
    -add query options on get
    -add post
    -get one with query options

--- server/src/controller/articleController.ts
+++ server/src/controller/articleController.ts
@@ -1,17 +1,61 @@
 import { Router, type Request, type Response } from "express";
-import { getAllArticles } from '../services/articleService.js'
+import { createArticle, getAllArticles, getArticle } from '../services/articleService.js'
+import { isAdmin, isAuth } from "../middlewares/authMiddleware.js";
+import type { ArticlePayload, ArticleQueryOptions } from "../types/article.types.js";
 
 const articleController = Router();
 
 articleController.get('/', async (req: Request, res: Response) => {
     try {
-        const articles = await getAllArticles();
-        res.status(200).json({ data: articles })
+        const queryOptions: ArticleQueryOptions = {
+            sortBy: req.query.sortBy as string,
+            pageSize: req.query.pageSize as string,
+            load: req.query.load as string
+        };
+
+        const articles = await getAllArticles(queryOptions);
+        res.status(200).json({ results: articles.length, data: articles })
     } catch (error: any) {
         res.status(500).json({
             message: error.message || "Server error!"
         })
     }
 });
+
+articleController.post('/', isAuth, isAdmin, async (req: Request, res: Response) => {
+    const { title, content, image, category }: ArticlePayload = req.body;
+    const currentUser = req.user!;
+    try {
+        const article = await createArticle({ title, content, image, category, author: currentUser._id })
+        res.status(201).json({
+            message: 'Article created.',
+            data: article
+        })
+    } catch (error: any) {
+        res.status(400).json({
+            message: error.message || 'Something went wrong during creation.'
+        })
+    }
+})
+
+articleController.get('/:id', async (req: Request, res: Response) => {
+    try {
+        const articleId = req.params.id;
+
+        const queryOptions: ArticleQueryOptions = {
+            load: req.query.load as string
+        };
+
+        const article = await getArticle(articleId as string, queryOptions)
+        res.status(200).json({
+            message: 'Article fetched',
+            data: article
+        })
+    } catch (error: any) {
+        res.status(500).json({
+            message: error.message || 'Server error.'
+        })
+    }
+})
 
 export default articleController;
