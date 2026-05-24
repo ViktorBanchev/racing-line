1efb6c867a4b3a585dbf3f22ad174cbe8d23dbcd
Vikicha089 <vbanchevf@gmail.com>
2026-05-24 21:46:39 +0300

    Create routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,10 @@
 import { Router } from "express";
+import articleController from "./controller/articleController.js";
+import authController from "./controller/authController.js";
 
 const routes = Router();
+
+routes.use('/articles', articleController);
+routes.use('/auth', authController);
 
 export default routes;
