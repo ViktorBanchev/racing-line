c723afe1432818561a25fa7cf2e482a1e1bb8858
Vikicha089 <vbanchevf@gmail.com>
2026-05-25 19:10:32 +0300

    Add comments to routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import articleController from "./controller/articleController.js";
 import authController from "./controller/authController.js";
+import commentController from "./controller/commentController.js";
 
 const routes = Router();
 
 routes.use('/articles', articleController);
 routes.use('/auth', authController);
+routes.use('/comments', commentController);
 
 export default routes;
