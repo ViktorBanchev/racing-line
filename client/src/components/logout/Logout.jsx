a7affc165d0df285d250adc0227365d40db32dc6
Vikicha089 <vbanchevf@gmail.com>
2026-05-25 19:16:24 +0300

    Update Logout.jsx

--- client/src/components/logout/Logout.jsx
+++ client/src/components/logout/Logout.jsx
@@ -1,20 +1,23 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { useNavigate } from "react-router";
 import UserContext from "../../contexts/userContext.jsx";
 import { toast } from "react-toastify";
 
 export default function Logout() {
     const navigate = useNavigate();
-    const {logoutHandler} = useContext(UserContext);
-    logoutHandler()
-        .then(() => {
+    const { logoutHandler } = useContext(UserContext);
+
+    useEffect(() => {
+        try {
+            logoutHandler()
             navigate('/')
-            toast.success('Logout successful!', {autoClose: 2000});
-        })
-        .catch(() => {
+            toast.success('Logout successful!', { autoClose: 2000 });
+
+        } catch {
             toast.error('Problem with logout!');
             navigate('/');
-        })
+        }
+    }, [logoutHandler, navigate])
 
     return null;
 }
